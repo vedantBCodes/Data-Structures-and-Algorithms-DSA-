467ab75ae2008e8984c95b9ba8d1e65595401f72
vedant bichkunde <vedantbichkunde@vedants-MacBook-Air.local>
2023-08-21 11:01:02 +0530

    printing natural numbers using recursion

---
+++ Recurrsion/printingFirstnNaturalNumbers.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+ 
+void print(int num)
+{
+    if(num==0)
+    {
+        return;
+    }
+    cout<<num<<" ";
+    print(num-1);
+}
+int main()
+{
+    int num;
+    cout<<"Enter a number:";
+    cin>>num;
+    print(num);
+   return 0;
+}
