f2b4d185fe5439ed08907022cc0d2028536f3923
vedant Yevatikar <bichkundevedant97@gmail.com>
2024-10-03 17:37:12 +0530

    optimizing code

--- Algorithms on Arrays/bubbleSort.cpp
+++ Algorithms on Arrays/bubbleSort.cpp
@@ -15,12 +15,18 @@ void bubbleSort1(int arr[],int n)
 {
     for(int i=0;i<n;i++)
     {
+        bool swapped=false;
         for(int j=0;j<n-1-i;j++)
         {
             if(arr[j]>arr[j+1])
             {
                 swap(arr[j],arr[j+1]);
+                swapped=true;
             }
+        }
+        if(swapped==false)
+        {
+            break;
         }
     }
 }
